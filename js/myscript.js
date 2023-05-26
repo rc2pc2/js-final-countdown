953b07f744825518315e613a9f1fb53b8584cec9
rc2pc2 <petriccarcc@gmail.com>
2023-05-26 10:50:27 +0200

    added weekend calculation feature to the countdown

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,26 @@
 const clock = setInterval(countDown, 1000);
 
 function countDown(){
-   const nextLessonDate = new Date("May 26, 2023 10:35:00");
-   const nowDate = Date.now();
-   const timeDifference = nextLessonDate.getTime() - nowDate;
-
    const seconds = 1000;
    const minute = seconds * 60;
    const hour = minute * 60;
    const day = hour * 24;
+
+   const nowDate = new Date();
+   let nextLessonDate = new Date( nowDate.getTime() + day) ;
+
+   if ( nextLessonDate.getDay() === 0 ) {
+      nextLessonDate = new Date( nowDate.getTime() + (day*2));
+   } else if ( nextLessonDate.getDay() === 6){
+      nextLessonDate = new Date( nowDate.getTime() + (day*3));
+   }
+
+   nextLessonDate.setHours(9);
+   nextLessonDate.setMinutes(30);
+   nextLessonDate.setSeconds(0);
+   nextLessonDate.setMilliseconds(0);
+
+   const timeDifference = nextLessonDate.getTime() - nowDate;
 
    const missingSeconds = Math.floor((timeDifference % minute) / 1000);
    updateDigits('div#seconds-counter p.counter-digits', missingSeconds);
@@ -16,14 +28,14 @@ function countDown(){
    const missingMinutes = Math.floor( (timeDifference % hour) / minute );
    updateDigits('div#minutes-counter p.counter-digits', missingMinutes);
 
-   const missingHours = Math.floor( ( timeDifference % (hour * 60)) / hour );
+   const missingHours = Math.floor( ( timeDifference % (hour * 24)) / hour );
    updateDigits('div#hours-counter p.counter-digits', missingHours);
 
    const missingDays = Math.floor( ( timeDifference % (day * 24) ) / day);
    updateDigits('div#days-counter p.counter-digits', missingDays);
 
 
-   if ( timeDifference <= 0){
+   if ( timeDifference <= 999){
       clearInterval(clock);
    }
 }
